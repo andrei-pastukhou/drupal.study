85236ad9d80c719d243e903d4267f6f69a36a3d6
Andrei Pastukhou <Andrei_Pastukhou@epam.com>
2017-09-22 16:37:05 +0300

    task3 is ready

---
+++ sites/all/modules/custom/training3/js/training3.js
@@ -0,0 +1,33 @@
+(function($) {
+  Drupal.behaviors.onload_page = {
+    attach: function (context, settings){
+      update_table_full (context, settings);
+    }
+  };
+
+  // Function redraw table.
+  function update_table_full(context, settings)
+  {
+    if (settings['training3_file_rows_table']) {
+      $('#training3-table-files').find("tr:gt(0)").remove();
+      row_array = settings['training3_file_rows_table'];
+      row_array.forEach(function (value, index, array) {
+        //console.log (settings['training3_user_id'] + value.data.user_id);
+        if(Number(settings['training3_user_id']) == Number(value.data.user_id)){
+          style = 'class = "tr3_user_file"';
+        }
+        else {
+          style = '';
+        }
+        $('#training3-table-files > tbody:last').append('<tr ' + style + '>' +
+            '<td>' + (index + 1) + '</td>' +
+            '<td>' + value.data.user_id + '</td>' +
+            '<td>' + value.data.path + '</td>' +
+            '<td>' + value.data.status + '</td>' +
+            '<td>' + value.data.url + '</td>' +
+            '</tr>');
+
+      }); // End forEach.
+    } // End if.
+  }
+})(jQuery);
